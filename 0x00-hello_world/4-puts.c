244caa1fbb74cf28a99f7970c2d1d66eca35e25f
silasoladipo <silasuxer@gmail.com>
2022-09-08 16:17:58 -0700

    T

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,20 +4,22 @@
 
 /**
  *
- *  * main - Entry point
+ * * main - Printing
  *
- *   *
+ * *
  *
- *    * Return: Always 0 (Success)
+ * * Return: Always 0 (Successful)
  *
- *     */
+ * */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual
 
-			return (0);
+			puzzle");
+
+	return (0);
 
 }
