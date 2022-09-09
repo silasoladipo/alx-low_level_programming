2e7447f000b398dc0386d3af7cd9f63d3272bd95
silasoladipo <silasuxer@gmail.com>
2022-09-08 17:21:11 -0700

    Task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,19 @@
+vim 5-printf.c
 #include <stdio.h>
+
 
 /**
  *
- *  * main - Entry point
+ * * main - Prints a text
  *
- *   *
+ * *
  *
- *    * Return: Always 0 (Success)
+ * * Return: Always 0 (Success)
  *
- *     */
+ * */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of
+			art,\n");
+	return (0);
 }
