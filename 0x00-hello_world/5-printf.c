8e68b94fa71a48eef42c55e4c0d0f204c6485d03
silasoladipo <silasuxer@gmail.com>
2022-09-08 17:18:29 -0700

    Task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - Entry point
@@ -11,13 +9,8 @@
  *    * Return: Always 0 (Success)
  *
  *     */
-
 int main(void)
-
 {
-
 		printf("with proper grammar, but the outcome is a piece of art,\n");
-
 			return (0);
-
 }
